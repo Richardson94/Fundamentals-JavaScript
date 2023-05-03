f0066b0154e06b22b8f05dfb07c6648bd65965be
Richardson94 <richcori94@gmail.com>
2023-05-03 11:34:58 -0400

    solution of the fourth exercise

---
+++ exerciseFour.js
@@ -0,0 +1,30 @@
+//task 1
+for (var i = 1; i <= 5; i++) {
+  console.log(i);
+}
+console.log('Counting completed!');
+//task 2
+for (var i = 5; i > 0; i--) {
+  console.log(i);
+}
+console.log('Countdown finished!');
+//task 3
+var i = 1;
+while (i < 6) {
+  console.log(i);
+  i++;
+}
+console.log('Counting completed!');
+//task 4
+var i = 5;
+while (i > 0) {
+  console.log(i);
+  i = i - 1;
+}
+console.log('Counting completed!');
+//task 5
+var year = 2018;
+while (year < 2023) {
+  console.log(year);
+  year++;
+}
